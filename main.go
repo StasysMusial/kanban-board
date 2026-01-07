eb3901a6a3541014cda4e3229622680d4723275f
StasysMusial <oderauchnicht@me.com>
2026-01-07 17:51:37 +0100

    created simple scrolling list

--- main.go
+++ main.go
@@ -1,12 +1,59 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"strconv"
+
 	tea "github.com/charmbracelet/bubbletea"
-	lip "github.com/charmbracelet/lipgloss"
 )
 
 func main() {
-	list := List{
-		content: []string{},
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("error: %v", err)
+		os.Exit(1)
 	}
+
+}
+
+type model struct {
+	list List
+}
+
+func initialModel() tea.Model {
+	var content []string = []string{}
+	for i := range 64 {
+		content = append(
+			content,
+			fmt.Sprintf("test line %s", strconv.Itoa(i)),
+		)
+	}
+	return model{
+		list: NewList(30, 10, content),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	m.list, cmd = m.list.Update(msg)
+
+	return m, cmd
+}
+
+func (m model) View() string {
+	return m.list.View()
 }
